046c2db948f1474e488fc81fb088b0a4aaa34d8f
asheshwor <asheshwor@gmail.com>
2015-07-06 19:23:22 +0930

    :earth_asia: added missing end quotes

--- ui.R
+++ ui.R
@@ -40,9 +40,9 @@ body <- dashboardBody(
                          # collapsible=TRUE,
                          dateRangeInput("daterange", "Select date range:",
                                         start = "2015-04-12",
-                                        end   = "2015-07-06,
+                                        end   = "2015-07-06",
                                         min = "2015-04-12",
-                                        max = "2015-07-06),
+                                        max = "2015-07-06"),
                          actionButton("updateButton", "Update graphs")
                          # verbatimTextOutput("adf")
                          )),
