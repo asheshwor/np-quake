f135babd6fc078ec3c181636a5b610ac53db7414
asheshwor <asheshwor@gmail.com>
2015-07-22 10:45:47 +0930

    :octocat: fixed date input value

--- ui.R
+++ ui.R
@@ -40,9 +40,9 @@ body <- dashboardBody(
                          # collapsible=TRUE,
                          dateRangeInput("daterange", "Select date range:",
                                         start = "2015-04-12",
-                                        end   = "2015-06-13",
+                                        end   = "2015-07-07",
                                         min = "2015-04-12",
-                                        max = "2015-06-13"),
+                                        max = "2015-07-07"),
                          actionButton("updateButton", "Update graphs")
                          # verbatimTextOutput("adf")
                          )),
