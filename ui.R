a29b064d21c0f693386e79e8f6ca050024a9ffa0
asheshwor <asheshwor@gmail.com>
2015-10-10 23:03:59 +1030

    :earth_asia: Updated quake data

--- ui.R
+++ ui.R
@@ -40,9 +40,9 @@ body <- dashboardBody(
                          # collapsible=TRUE,
                          dateRangeInput("daterange", "Select date range:",
                                         start = "2015-04-12",
-                                        end   = "2015-09-22",
+                                        end   = "2015-10-10",
                                         min = "2015-04-12",
-                                        max = "2015-09-22"),
+                                        max = "2015-10-10"),
                          actionButton("updateButton", "Update graphs")
                          # verbatimTextOutput("adf")
                          )),
